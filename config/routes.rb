706a64c9f0c3264e6e06e43cebb324dcebb7f3b9
tatsuyafukui <nakedtatsuya@gmail.com>
2021-04-28 18:31:03 +0900

    end

--- config/routes.rb
+++ config/routes.rb
@@ -19,10 +19,8 @@ Rails.application.routes.draw do
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
   get "posts/:parent_id/thread" => "posts#thread_new"
-  
   post "posts/create" => "posts#create"
   post "posts/:parent_id" => "posts#thread_create"
-  
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
--- spec/requests/step3_spec.rb
+++ spec/requests/step3_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "User likes", type: :request do
+RSpec.describe "Posts", type: :request do
   let(:user_params) { { name: 'にんじゃわんこ', email: 'wanko@prog-8.com', password: 'wanko' } }
   let(:user) { User.create(user_params) }
-
   let(:post_params) { { content: 'parent post', user_id: user.id } }
+  before(:each) { allow_any_instance_of(ApplicationController).to receive(:session).and_return(user_id: user.id) }
 
-
-  context 'loggedin' do
-    before(:each) { allow_any_instance_of(ApplicationController).to receive(:session).and_return(user_id: user.id) }
-
+  context 'create' do
     it '@@@新規投稿でエラーが発生しています@@@' do
       post '/posts/create', params: post_params
       expect(response).to have_http_status(302)
@@ -18,6 +15,20 @@ RSpec.describe "User likes", type: :request do
     it '@@@新規投稿後は投稿一覧画面にリダイレクトしてください@@@' do
       post '/posts/create', params: post_params
       expect(response).to redirect_to("/posts/index")
+    end
+  end
+
+  context 'thread create' do
+    it '@@@新規スレッド投稿でエラーが発生しています@@@' do
+      parent_post = Post.create(content: 'test', user_id: user.id)
+      post "/posts/#{parent_post.id}", params: post_params
+      expect(response).to have_http_status(302)
+    end
+
+    it '@@@新規スレッド投稿でエラーが発生しています@@@' do
+      parent_post = Post.create(content: 'test', user_id: user.id)
+      post "/posts/#{parent_post.id}", params: post_params
+      expect(Post.all.count).to eq(2)
     end
   end
 end
